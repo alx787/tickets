702dfb17ed0286513d6a2dfd045bddc45ed4c860
alex <kaanova@gmail.com>
2019-08-13 17:24:50 +0300

    add login page

--- src/main/java/ru/ath/asu/auth/UserInfo.java
+++ src/main/java/ru/ath/asu/auth/UserInfo.java
@@ -1,4 +1,43 @@
 package ru.ath.asu.auth;
 
 public class UserInfo {
+    private String email;
+    private String fio;
+    private String department;
+    private String phone;
+
+    public UserInfo() {
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFio() {
+        return fio;
+    }
+
+    public void setFio(String fio) {
+        this.fio = fio;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
 }
--- src/main/java/ru/ath/asu/portal/portalAction.java
+++ src/main/java/ru/ath/asu/portal/portalAction.java
@@ -4,13 +4,67 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import com.atlassian.jira.web.action.JiraWebActionSupport;
 
+
 public class portalAction extends JiraWebActionSupport
 {
     private static final Logger log = LoggerFactory.getLogger(portalAction.class);
 
+    private String userName;
+
     @Override
     public String execute() throws Exception {
 
-        return super.execute(); //returns SUCCESS
+        //return super.execute();
+        //log.warn(" ==== this is log");
+        super.execute();
+        return "login";
+    }
+
+    public String doDefault() throws Exception {
+        this.userName = "";
+        return "login";
+    }
+
+    public String doAuth() {
+
+        String loginName = getHttpRequest().getParameter("login-name");
+        String loginPass = getHttpRequest().getParameter("login-pass");
+
+        if (loginName == null) {
+            loginName = "";
+        }
+
+        this.userName = loginName;
+
+
+        // проверка логина пароля
+        if (checkPass(loginName, loginPass)) {
+            log.warn(" ===== авторизовался");
+            return "login";
+        }
+
+        log.warn(" ===== не авторизовался");
+        return "login";
+    }
+
+    // здесь проверка логина и пароля
+    private boolean checkPass(String login, String password) {
+        String uName = "alx";
+        String uPass = "123";
+
+        if (uName.equals(login) && uPass.equals(password)) {
+            return true;
+        }
+
+        return false;
+
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
     }
 }
