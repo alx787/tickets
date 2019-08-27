e61b6486145d5a9e50e9fa2984c499dd9740d4e6
alex <kaanova@gmail.com>
2019-08-27 14:41:59 +0300

    post data to server

--- src/main/java/ru/ath/asu/rest/TicketsRest.java
+++ src/main/java/ru/ath/asu/rest/TicketsRest.java
@@ -1,5 +1,7 @@
 package ru.ath.asu.rest;
 
+import com.atlassian.plugins.rest.common.multipart.FilePart;
+import com.atlassian.plugins.rest.common.multipart.MultipartFormParam;
 import com.atlassian.plugins.rest.common.security.AnonymousAllowed;
 
 import javax.ws.rs.*;
@@ -8,6 +10,13 @@ import javax.ws.rs.core.Response;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
 /**
  * A resource of message.
  */
@@ -22,10 +31,57 @@ public class TicketsRest {
     @Produces({MediaType.APPLICATION_JSON})
     @Consumes({MediaType.MULTIPART_FORM_DATA})
     @Path("/createticket")
-    public Response createIssueFromTicket(String inputJson)
+    public Response createIssueFromTicket(@MultipartFormParam("ticket-theme") FilePart ticketTheme,
+                                          @MultipartFormParam("ticket-text") FilePart ticketText,
+                                          @MultipartFormParam("ticket-file-upload") List<FilePart> filePartList)
+
+
     {
         log.warn("============= in ws ");
-        log.warn(inputJson);
+        log.warn(ticketTheme.getValue());
+        log.warn(ticketText.getValue());
+
+
+        for (FilePart filePart : filePartList) {
+            String fileName = filePart.getName();
+            try {
+                InputStream is = filePart.getInputStream();
+
+                File file = new File("/tmp/" + fileName);
+
+                try (FileOutputStream outputStream = new FileOutputStream(file)) {
+
+                    int read;
+                    byte[] bytes = new byte[1024];
+
+                    while ((read = is.read(bytes)) != -1) {
+                        outputStream.write(bytes, 0, read);
+                    }
+
+                }
+
+
+
+//                try (InputStream inputStream = u.toURL().openStream()) {
+//
+//                    File file = new File(FILE_TO);
+//
+//                    // commons-io
+//                    FileUtils.copyInputStreamToFile(inputStream, file);
+//                }
+
+
+
+
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            log.warn(fileName);
+
+
+        }
 
         return Response.ok("[]").build();
     }
--- src/main/resources/js/ticketnew.js
+++ src/main/resources/js/ticketnew.js
@@ -4,104 +4,28 @@ createticket.module = (function () {
 
     var createIssue = function() {
 
-
-//
-//         Ajax/Jquery
-//
-//         $(".uploadDocumentGeneral").on("click", function (evt) {
-//
-//             var documentData = new FormData();
-//             documentData.append('file', $('input#file.findDocumentOnboarding')[0].files[0]);
-//             $.ajax({
-//                 url: url,
-//                 type: 'POST',
-//                 data: documentData,
-//                 cache: false,
-//                 contentType: false,
-//                 processData: false,
-//                 success: function (response) {
-//                     alert("Document uploaded successfully.");
-//                 }
-//             });
-//
-//             return false;
-//         });
-//
-//         JAVA
-//
-// @POST
-// @Path("upload/{id}")
-// @Consumes({"application/x-www-form-urlencoded", "multipart/form-data"})
-//
-//         public void addBlob(@PathParam("id") Integer id, @FormDataParam("file") InputStream uploadedInputStream) throws IOException {
-//             E24ClientTemp entityToMerge = find(id);
-//             try {
-//                 ByteArrayOutputStream out = new ByteArrayOutputStream();
-//                 int read = 0;
-//                 byte[] bytes = new byte[1024];
-//                 while ((read = uploadedInputStream.read(bytes)) != -1) {
-//                     out.write(bytes, 0, read);
-//                 }
-//                 entityToMerge.setTestBlob(out.toByteArray());
-//                 super.edit(entityToMerge);
-//             }
-//             catch (IOException e) {
-//                 e.printStackTrace();
-//             }
-//         }
-
-
-
-
-//         var formData = new FormData();
-//         formData.append('logo', logoImg);
-//         var objArr = [];
-//
-//         objArr.push({"id": id, "name": userName});
-//
-// //JSON obj
-//         formData.append('objArr', JSON.stringify( objArr ))l
-//
-//
-//         $.ajax({
-//             url: url,
-//             type:"POST",
-//             processData:false,
-//             contentType: false,
-//             data: formData,
-//             complete: function(data){
-//                 alert("success");
-//             }
-//         });
-//         Async is a bad idea that is deprecated by many browsers, and cache is not needed for POSTs.
-//
-//                                                                                                  JΛ̊KE
-
+        // на время заблокируем кнопку
+        AJS.$("#ticket-form .buttons button").attr("disabled", "disabled");
+        // AJS.$("#ticket-form .buttons button").removeAttr("disabled");
 
 
         var restUrl = AJS.params.baseURL + "/rest/exploretickets/1.0/service/createticket";
-        var jsonData = {
-            "fields": {
-                "project":
-                    {
-                        "key": "ZVK",
-                    },
-                "summary": AJS.$("#ticket-theme").val(),
-                "description": AJS.$("#ticket-input").val(),
-                "issuetype": {
-                    "id": "10000",
-                }
-            }
-        };
 
-        // var formDataTicket = new FormData();
-        // formDataTicket.append("ticket-theme", AJS.$("#ticket-theme").val());
-        // formDataTicket.append("ticket-text", AJS.$("#ticket-text").val());
-        // formDataTicket.append("ticket-file-upload", AJS.$("#ticket-file-upload")[0].files);
+        var formDataTicket = new FormData();
 
-        var form = AJS.$("#ticket-form")[0];
-        // Create an FormData object
-        var formDataTicket = new FormData(form);
+        formDataTicket.append("ticket-theme", AJS.$("#ticket-theme").val());
+        formDataTicket.append("ticket-text", AJS.$("#ticket-text").val());
+
+        var fileObj = AJS.$("#ticket-file-upload")[0].files;
+        var fileLen =  fileObj.length;
+        for (var i = 0; i < fileLen; i++) {
+            formDataTicket.append("ticket-file-upload", fileObj[i]);
+
+        }
+
+
+        // var form = AJS.$("#ticket-form")[0];
+        // var formDataTicket = new FormData(form);
 
 
         AJS.$.ajax({
