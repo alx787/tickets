a5f337a017e9e7b6f33eeec9aebddd661006b919
alex <kaanova@gmail.com>
2019-12-18 17:06:29 +0300

    added ao 2

--- src/main/java/ru/ath/asu/tickets/aousers/TicketUserDao.java
+++ src/main/java/ru/ath/asu/tickets/aousers/TicketUserDao.java
@@ -3,6 +3,8 @@ package ru.ath.asu.tickets.aousers;
 public interface TicketUserDao {
     TicketUser findById(int id);
     TicketUser[] findByAll();
+//    TicketUser findByLoginPassword();
     TicketUser create(String login, String username, String depart, String password);
     void remove(TicketUser ticketUser);
+//    void update(int id, TicketUser ticketUser);
 }
